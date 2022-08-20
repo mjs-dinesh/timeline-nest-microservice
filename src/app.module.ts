aca7939764af1dda745a01fa584d1e2ae8d65a5f
mjs-dinesh <dineshmjs22@gmail.com>
2022-08-20 17:39:24 +0530

    auth user security module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,35 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule, MongooseModuleAsyncOptions } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CONFIG_VALIDATION_OPTION, CONFIG_VALIDATION_SCHEMA } from './common_utils/constant';
+import {
+  CONFIG_VALIDATION_OPTION,
+  CONFIG_VALIDATION_SCHEMA,
+} from './common_utils/constant';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { UserSecurityModule } from './modules/user_security/user_security.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: CONFIG_VALIDATION_SCHEMA,
-      validationOptions: CONFIG_VALIDATION_OPTION
+      validationOptions: CONFIG_VALIDATION_OPTION,
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) =>
-      ({
-        uri: configService.get('DB_URL'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      } as MongooseModuleAsyncOptions),
+        ({
+          uri: configService.get('DB_URL'),
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        } as MongooseModuleAsyncOptions),
     }),
+    AuthModule,
+    UserModule,
+    UserSecurityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
