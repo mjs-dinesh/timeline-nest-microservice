662d38f51b10c092e5b811f6d640f18e91e070b6
mjs-dinesh <dineshmjs22@gmail.com>
2022-08-20 17:37:40 +0530

    filters added

--- src/utils/filters/not_found_exception.filter.ts
+++ src/utils/filters/not_found_exception.filter.ts
@@ -1,11 +1,13 @@
-import { Catch, ExceptionFilter, NotFoundException } from '@nestjs/common'
+import { Catch, ExceptionFilter, NotFoundException } from '@nestjs/common';
 
 @Catch(NotFoundException)
 export class NotFoundExceptionFilter implements ExceptionFilter {
   catch(exception: NotFoundException) {
+    let response = {};
+    response = exception.getResponse() || { message: exception.message };
     return {
       statusCode: 404,
-      message: exception.message,
-    }
+      ...response,
+    };
   }
 }
---
+++ src/utils/filters/unauthorized_exception.ts
@@ -0,0 +1,13 @@
+import { Catch, ExceptionFilter, HttpStatus, UnauthorizedException } from '@nestjs/common';
+
+@Catch(UnauthorizedException)
+export class UnauthorizedExceptionFilter implements ExceptionFilter {
+  catch(exception: UnauthorizedException) {
+    let response = {};
+    response = exception.getResponse() || { message: exception.message };
+    return {
+      statusCode: HttpStatus.UNAUTHORIZED,
+      ...response,
+    };
+  }
+}
