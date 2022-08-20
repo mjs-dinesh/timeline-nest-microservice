968a2cd11e3d05569ef281167cad59d395e475f6
mjs-dinesh <dineshmjs22@gmail.com>
2022-08-20 17:39:52 +0530

    global filters added

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
-import { ValidationPipeOptions } from './common_utils/helpers/validation_pipe.helper';
-import { MongoExceptionFilter, ValidationFilter } from './utils/filters';
+import { ValidationPipeOptions } from './utils/helper/validation_pipe.helper';
+import {
+  MongoExceptionFilter,
+  NotFoundExceptionFilter,
+  ValidationFilter,
+} from './utils/filters';
+import { UnauthorizedExceptionFilter } from './utils/filters/unauthorized_exception';
 
 async function bootstrap() {
-  const configService: ConfigService = new ConfigService()
+  const configService: ConfigService = new ConfigService();
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
@@ -16,10 +22,16 @@ async function bootstrap() {
         host: configService.get('HOST'),
         port: configService.get('PORT'),
       },
-    }
-  )
-  app.useGlobalPipes(new ValidationPipe(ValidationPipeOptions))
-  app.useGlobalFilters(new MongoExceptionFilter(), new ValidationFilter())
+    },
+  );
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.useGlobalPipes(new ValidationPipe(ValidationPipeOptions));
+  app.useGlobalFilters(
+    new MongoExceptionFilter(),
+    new ValidationFilter(),
+    new NotFoundExceptionFilter(),
+    new UnauthorizedExceptionFilter(),
+  );
   await app.listen();
 }
 bootstrap();
