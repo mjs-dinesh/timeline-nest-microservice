ff738cb8479ee145546d17e4fd688c736613fe8c
mjs-dinesh <dineshmjs22@gmail.com>
2022-08-20 17:38:08 +0530

    utils helper added

---
+++ src/utils/helper/interface.helper.ts
@@ -0,0 +1,7 @@
+export type tokenType = 'short' | 'long' | 'medium';
+
+export enum TOKEN_TYPE {
+  SHORT = 'JWT_SHORT',
+  MEDIUM = 'JWT_MEDIUM',
+  LONG = 'JWT_LONG',
+}
---
+++ src/utils/helper/validation_pipe.helper.ts
@@ -0,0 +1,10 @@
+import { ValidationError } from '@nestjs/common'
+import { ValidationErrorException } from '../exceptions'
+
+export const ValidationPipeOptions = {
+  transform: true,
+  whitelist: true,
+  exceptionFactory: (validationErrors: ValidationError[] = []) => {
+    return new ValidationErrorException(validationErrors)
+  },
+}
